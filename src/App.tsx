e777727aabc6b92433e6eaa9f60c2a94b0f4f0c6
Claudie Gueguen <gueguen_c@yahoo.fr>
2023-12-12 17:47:59 +0100

    not found page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "./components/Main";
+import NotFound from "./components/NotFound";
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" Component={Main} />
+        <Route path="*" Component={NotFound} />
       </Routes>
     </Router>
   );
